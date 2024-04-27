aaf08af923add5f921e739d734bc90a7b2b4e6d6
aaqib <aqibnengroo5@gmail.com>
2024-04-27 16:28:40 +0530

    'js'

--- main.js
+++ main.js
@@ -17,21 +17,23 @@
 // let bonuspoint=50;
 // console.log(bonuspoint)
 let count=0;
-let svarr=new Array();
+// let svarr=new Array();
+let prevalue=document.getElementById("previous-score");
+let value=document.getElementById("counter");
 function increment(){
    count+=1;
    console.log(count);
-   let value=document.getElementById("counter");
-   value.innerHTML=count;
+   value.textContent=count;
 }
 function save(){
-    let savedvalue=count+" -";
-    svarr.push(savedvalue);
-    console.log('length '+svarr.length);
+    let savedvalue=count+" - ";
+    // svarr.push(savedvalue);
+    // console.log('length '+svarr.length);
     console.log('savedvalue: '+savedvalue);
-    let prevalue=document.getElementById("previous-score");
-    // prevalue.innerText="Previous entries-->"
-    prevalue.innerText+=savedvalue;
+    prevalue.textContent+=savedvalue;
+    count=0
+    value.textContent=count;
+     // prevalue.innerText="Previous entries-->"
     // for(let i=0;i<Math.min(3,svarr.length);i++){
     //     prevalue.innerText+=`${svarr[i]}`;            
     //     if(i<svarr.length-1){
